ed083ed7f103b3fa0f7afc893d696996a97b93ff
Yanira Monsalve <yaniramonsalvef@gmail.com>
2021-12-01 11:43:41 -0500

    Crear card view

---
+++ app/src/main/java/com/yaniramonsalve/baricharaturistica/ListBaricharaActivity.kt
@@ -0,0 +1,11 @@
+package com.yaniramonsalve.baricharaturistica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListBaricharaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_barichara)
+    }
+}
