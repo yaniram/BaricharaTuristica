74bfaf612c75c07f95fc333f58995d41a405f43f
Yanira Monsalve <yaniramonsalvef@gmail.com>
2021-12-01 17:08:49 -0500

    Recycler view

---
+++ app/src/main/java/com/yaniramonsalve/baricharaturistica/BaricharaItem.kt
@@ -0,0 +1,9 @@
+package com.yaniramonsalve.baricharaturistica
+
+data class BaricharaItem(
+    val urlFoto: String,
+    val name: String,
+    val introduction: String,
+    val valoration : String,
+
+)
